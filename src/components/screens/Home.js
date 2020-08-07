5aebf13a9fca7694dd84b712e251ebc1f5404f7c
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2020-08-07 17:27:09 -0400

    Pages have JS now

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 function Home(props){
+    const [skill, setSkill] = React.useState(1)
+
     const disabled_skill_style="wave-effect col s1"
     const active_skill_style="active col s1"
 
@@ -17,20 +19,20 @@ function Home(props){
             </div>
 
             <ul className="pagination row">
-                <li className="disabled col s1">            <a href="#!">           <i class="material-icons">chevron_left</i>      </a>        </li>
+                <li className="disabled col s1" onClick={()=>setSkill(skill-1)}>            <a href="#!">           <i class="material-icons">chevron_left</i>      </a>        </li>
 
-                <li className={active_skill_style}>         <a href="#!">1</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">2</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">3</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">4</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">5</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">6</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">7</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">8</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#!">9</a>      </li>
-                <li className={disabled_skill_style}>       <a href="#1">10</a>     </li>
+                <li className={skill === 1 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(1)}>        <a href="#!">1</a>      </li>
+                <li className={skill === 2 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(2)}>        <a href="#!">2</a>      </li>
+                <li className={skill === 3 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(3)}>        <a href="#!">3</a>      </li>
+                <li className={skill === 4 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(4)}>        <a href="#!">4</a>      </li>
+                <li className={skill === 5 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(5)}>        <a href="#!">5</a>      </li>
+                <li className={skill === 6 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(6)}>        <a href="#!">6</a>      </li>
+                <li className={skill === 7 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(7)}>        <a href="#!">7</a>      </li>
+                <li className={skill === 8 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(8)}>        <a href="#!">8</a>      </li>
+                <li className={skill === 9 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(9)}>        <a href="#!">9</a>      </li>
+                <li className={skill === 10 ? active_skill_style : disabled_skill_style} onClick={()=>setSkill(10)}>       <a href="#1">10</a>     </li>
 
-                <li className="active col s1">              <a href="#!">           <i class="material-icons">chevron_right</i>     </a>        </li>
+                <li className="active col s1" onClick={()=>setSkill(skill+1)}>              <a href="#!">           <i class="material-icons">chevron_right</i>     </a>        </li>
             </ul>
         </div>
     )
