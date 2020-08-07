5d196909bd6282557a288e4747967f4cbde2ef10
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2020-08-07 15:30:55 -0400

    Home Page Updated
    Broken materialize tab card removed
    
    Name card added inline padding

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -4,30 +4,7 @@ function Home(props){
     return(
         <div>
             <div className="card">
-                <h1>Nathan Melnyk</h1>
-                <p>About Me</p>
-            </div>
-
-            <div className="card">
-                <div className="card-content">
-                    My Skills
-                </div>
-
-                <div className="card-tabs">
-                    <ul className="tabs tabs-fixed-width">
-                        <li className="tab"><a href="#skill one">Skill One</a></li>
-                        <li className="tab"><a href="#skill two">Skill Two</a></li>
-                        <li className="tab"><a href="#skill three">Skill Three</a></li>
-                        <li className="tab"><a href="#...">...</a></li>
-                    </ul>
-                </div>
-
-                <div class="card-content">
-                    <div id="skill one">Skill One</div>
-                    <div id="skill two">Skill Two</div>
-                    <div id="skill three">Skill Three</div>
-                    <div id="...">...</div>
-                </div>
+                <h1 style={{padding:10}}>Nathan Melnyk</h1>
             </div>
 
         </div>
