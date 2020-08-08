e26f90613eadc9ad8aa8b066023b0acd64558246
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2020-08-08 19:23:44 -0400

    Skill card
    Pages dynamically change the Skill Card now

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -19,16 +19,105 @@ function Home(props){
         }
     }
 
+    function displaySkill(skill){
+        switch(skill){
+            case 1:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Skills</span>
+                        <p>These are the various skills I've picked up throughout college and independent studies!</p>
+                    </div>
+                )
+            case 2:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Materialize</span>
+                        <p>If it weren't obvious from the layout of this website, I've taught myself how to
+                            use the Materialize CSS library! It's sleek design and ease of use makes it one
+                            of my favorite CSS libraries to use!
+                        </p>
+                    </div>
+                )
+            case 3:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">React</span>
+                        <p>The other main library this website is built with! I learned React programming in 
+                            my CSE 316 class, as well as two courses on Scrimba. I fell in love with the idea of
+                            components and state, as it makes organizing and designing websites so much easier.
+                        </p>
+                    </div>
+                )
+            case 4:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Redux</span>
+                        <p>Some text here</p>
+                    </div>
+                )
+            case 5:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">MySQL/SQLite</span>
+                        <p>Some text here</p>
+                    </div>
+                )
+            case 6:
+                return(
+                    <div style={{padding:10}}>
+                        <span classname="card-title">MongoDB</span>
+                        <p>Some text here</p>
+                    </div>
+                )
+            case 7:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Github</span>
+                        <p>Some text here</p>
+                    </div>
+                )
+            case 8:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Node.JS</span>
+                        <p>Some text here</p>
+                    </div>
+                )
+            case 9: 
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Cron</span>
+                        <p>Some text here</p>
+                    </div>
+                )
+            case 10:
+                return(
+                    <div style={{padding:10}}>
+                        <span className="card-title">Programming Languages</span>
+                        <p>Java, Python, JS, Perl, HTML/CSS, VB</p>
+                    </div>
+                )
+            default:
+                return(
+                    <div>
+                        <span className="card-title">Skills</span>
+                        <p>These are the various skills I've picked up throughout college and independent studies!</p>
+                    </div>
+                )
+        }
+    }
+
     return(
         <div>
             <div className="card">
                 <h1 style={{padding:10}}>Nathan Melnyk</h1>
             </div>
 
+            {
+
+            }
             <div className="card ">
-                <div className="card-content">
-                    <span className="card-title">Skills</span>
-                </div>
+                {displaySkill(skill)}
             </div>
 
             <ul className="pagination card row">
