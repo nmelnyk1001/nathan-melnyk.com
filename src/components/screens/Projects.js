63c8fce2fca751933905049f5e41e7f5320efb3b
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2020-08-09 16:30:42 -0400

    Projects Screen
    Populuated with cards and project titles

--- src/components/screens/Projects.js
+++ src/components/screens/Projects.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 function Projects(props){
     return(
-        <div>
-            This is the projects screen
+        <div style={{padding:20}}>
+            <div className="card" style={{padding:10}}>
+                <span className="card-title">Personal Website</span>
+                <p>Some text here</p>
+            </div>
+            <div className="card" style={{ padding: 10 }}>
+                <span className="card-title">College 4 Me</span>
+                <p>Some text here</p>
+            </div>
+            <div className="card" style={{ padding: 10 }}>
+                <span className="card-title">Discord Birthday Bot</span>
+                <p>Some text here</p>
+            </div>
+            <div className="card" style={{ padding: 10 }}>
+                <span className="card-title">Financial Model Text Converter</span>
+                <p>Some text here</p>
+            </div>
+            <div className="card" style={{ padding: 10 }}>
+                <span className="card-title">Twitter Clone</span>
+                <p>Some text here</p>
+            </div>
         </div>
     )
 }
