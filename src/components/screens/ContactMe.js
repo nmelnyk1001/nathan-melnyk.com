f3011eaed842ae2201b3dccefdfbeafd7448489d
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2020-08-09 18:19:00 -0400

    Contact Me Screen filled
    Screen populated and Github portion styled

--- src/components/screens/ContactMe.js
+++ src/components/screens/ContactMe.js
@@ -1,9 +1,28 @@
 import React from "react"
 
 function ContactMe(props){
+    const textStyle={
+        display: "inline",
+        position: "absolute",
+        padding: "2em 2em"
+    }
+
     return(
-        <div>
-            Contact Me Page
+        <div style={{padding:20}}>
+            <div className="card" style={{padding:10}}>
+                <span className="card-title">Nathan Melnyk</span>
+                <p>Phone Number: +1(631)452-4263</p>
+                <p>East Setauket, New York</p>
+            </div>
+
+            <div className="card" style={{padding:10, position:"relative"}}>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github logo" height="100" width="100" style={{display:"inline"}}/>
+                <p style={textStyle}>https://github.com/nmelnyk1001</p>
+            </div>
+
+            <div className="card" style={{padding:10}}>
+                <img src="https://image.flaticon.com/icons/svg/281/281769.svg" alt="gmail logo" height="100" width="100"/>
+            </div>
         </div>
     )
 }
