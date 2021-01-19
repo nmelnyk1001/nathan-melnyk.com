3d185a2fe38dfc691250a5d515581cd6c650e63e
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2021-01-19 18:15:23 -0500

    Home Card Style Cleaned up

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -13,12 +13,12 @@ function Home(props){
     return(
         <div>
             <div className="card" style={{background: "#C2E1C2"}}>
-                <div className="card-content white-text" style={{ background:"#D7B870", padding:"5px"}}>
-                    <div className="row">
-                        <img className="col s4" style={{borderRadius:"50%", margin:"auto"}} src={headshot} alt="headshot" height="100%" width="100%"/>
+                <div className="card-content black-text" style={{ background:"#D7B870", padding:"5px"}}>
+                    <div className="row" style={{marginBottom:"0px"}}>
+                        <img className="col s4" style={{borderRadius:"50%", margin:"auto"}} src={headshot} alt="headshot" max-height="300px" height="100%" width="100%"/>
                         <div className="col s8">
-                            <h1 style={{color:"black"}}>Nathan Melnyk</h1>
-                            <p style={{color:"black"}}>
+                            <h1>Nathan Melnyk</h1>
+                            <p>
                                 Good day! I'm a college student attending Stonybrook University, set to graduate December 2020. 
                                 During my school career, I became the treasurer for the Science Fiction Forum for two years, budgeting for events, library items, and even our 50th anniversary.
                                 Outside of school I'm an avid athlete and craftsperson! 
@@ -34,10 +34,11 @@ function Home(props){
                     </div>
                 </div>
             </div>
+            {/*
             <div className="card" style={{ background: "#C2E1C2"}}>
                 {displaySkill(skill)}
             </div>
-            {/*
+            
             <ul className="pagination card row" style={{ background: "#C2E1C2"}}>
                 <li className={getPageStyle(1)} onClick={()=>setSkill(1)}>        <a href="#!" style={textStyle}>Materialize</a>      </li>
                 <li className={getPageStyle(2)} onClick={()=>setSkill(2)}>        <a href="#!">React</a>      </li>
