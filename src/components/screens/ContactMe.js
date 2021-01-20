ff741fdd790da470e4e2ca01474439ccd45c8e7c
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2021-01-20 17:29:02 -0500

    Contact Me Updated

--- src/components/screens/ContactMe.js
+++ src/components/screens/ContactMe.js
@@ -1,34 +1,29 @@
 import React from "react"
+import github from "../images/GitHub_Logo.png"
 
 function ContactMe(props){
-    const textStyle={
-        display: "inline",
-        position: "absolute",
-        padding: "2em 2em"
-    }
-
     return(
         <div>
-            <div className="card" style={{ padding: 10, background: "#C2E1C2"}}>
-                <span className="card-title">Nathan Melnyk</span>
-                <p>Phone Number: +1(631)452-4263</p>
-                <p>Long Island, New York</p>
+            <div style={{ padding: 10}}>
+                <p style={{ fontWeight: "bold", fontSize: "24px", marginLeft: "2%", marginBottom: "0.5%" }}>Nathan Melnyk</p>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "18px" }}>Phone Number: +1(631)452-4263</p>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "18px" }}>Long Island, New York</p>
             </div>
 
-            <div className="card" style={{ padding: 10, position: "relative", background: "#C2E1C2"}}>
-                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github logo" height="100" width="100" style={{display:"inline"}}/>
-                <p style={textStyle}>https://github.com/nmelnyk1001</p>
+            <div className="row" style={{ background: "#D7B870", paddingTop: "1.5%", paddingBottom: "1.5%" }}>
+                <img className="col s2" src={github} alt="github logo" height="100" width="100" style={{display:"inline"}}/>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "22px", display:"inline", position:"absolute", paddingTop:"2%"}}>https://github.com/nmelnyk1001</p>
             </div>
 
-            <div className="card" style={{ padding: 10, position: "relative", background: "#C2E1C2"}}>
-                <img src="https://image.flaticon.com/icons/svg/281/281769.svg" alt="gmail logo" height="100" width="100" style={{display:"inline"}}/>
-                <p style={textStyle}>nmelnyk1001@gmail.com</p>
+            <div className="row" style={{paddingTop: "1.5%", paddingBottom: "1.5%" }}>
+                <img className="col s2" src="https://image.flaticon.com/icons/svg/281/281769.svg" alt="gmail logo" height="100" width="100" style={{display:"inline"}}/>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "22px", display:"inline", position:"absolute", paddingTop:"2%"}}>nmelnyk1001@gmail.com</p>
                 {/*<textarea style={{display:"inline", width:"auto", height:"auto"}}></textarea>*/}
             </div>
 
-            <div className="card" style={{ padding: 10, position: "relative", background: "#C2E1C2"}}>
-                <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn Logo" height="100" width="100" style={{display:"inline"}}/>
-                <p style={textStyle}>www.linkedin.com/in/nathan-melnyk</p>
+            <div className="row" style={{ background: "#D7B870", paddingTop: "1.5%", paddingBottom: "1.5%" }}>
+                <img className="col s2" src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn Logo" height="100" width="100" style={{display:"inline"}}/>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "22px", display:"inline", position:"absolute", paddingTop:"2%"}}>www.linkedin.com/in/nathan-melnyk</p>
             </div>
         </div>
     )
