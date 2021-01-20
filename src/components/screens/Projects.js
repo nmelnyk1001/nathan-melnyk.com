f91a06363c13795922c8391df02976df507f27d5
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2021-01-20 17:12:48 -0500

    Project Screen Cleaned Up

--- src/components/screens/Projects.js
+++ src/components/screens/Projects.js
@@ -3,39 +3,49 @@ import React from 'react'
 function Projects(props){
     return(
         <div>
-            <div className="card" style={{ padding: 10, background: "#C2E1C2"}}>
-                <span className="card-title">Personal Website</span>
-                <p>
+            <div style={{paddingBottom:"1%", paddingTop:"0.5%"}}>
+                <div>
+                    <p style={{fontWeight:"bold", fontSize:"24px", marginLeft:"2%", marginBottom:"0.5%"}}>Personal Website</p>
+                </div>
+                <p style={{marginTop:"0%", marginLeft:"5%", fontSize:"18px"}}>
                     React based frontend, styled with Materialize CSS. Hosted on an instance from Google Cloud Platform.
                     SSL certified for HTTPS use.
                 </p>
             </div>
-            <div className="card" style={{ padding: 10, background: "#C2E1C2" }}>
-                <span className="card-title">College 4 Me</span>
-                <p>
+            <div style={{ background:"#D7B870", paddingTop:"1%", paddingBottom:"1%"}}>
+                <div>
+                    <p style={{ fontWeight: "bold", fontSize: "24px", marginLeft: "2%", marginBottom: "0.5%"}}>College 4 Me</p>
+                </div>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "18px" }}>
                     Four person group project.
                     College 4 Me is a web based app that generates college recommendations for student users. 
                     Converted group created wireframes into React based frontend.
                 </p>
             </div>
-            <div className="card" style={{ padding: 10, background: "#C2E1C2" }}>
-                <span className="card-title">Discord Birthday Bot</span>
-                <p>
+            <div style={{ paddingBottom: "1%", paddingTop: "0.5%" }}>
+                <div>
+                    <p style={{ fontWeight: "bold", fontSize: "24px", marginLeft: "2%", marginBottom: "0.5%" }}>Discord Birthday Bot</p>
+                </div>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "18px" }}>
                     Discord bot built using Discord.JS, hosted on Google Cloud Platform.
                     Allows users to input birthdays, which are stored in a SQLite database.
                     Cron runs scheduled tasks to congratulate users on their birthdays.
                 </p>
             </div>
-            <div className="card" style={{ padding: 10, background: "#C2E1C2" }}>
-                <span className="card-title">Financial Model Text Converter</span>
-                <p>
+            <div style={{ background:"#D7B870", paddingBottom: "1%", paddingTop: "0.5%" }}>
+                <div>
+                    <p style={{ fontWeight: "bold", fontSize: "24px", marginLeft: "2%", marginBottom: "0.5%" }}>Financial Model Text Converter</p>
+                </div>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "18px" }}>
                     Excel macro written in Visual Basic, commissioned by Torreya Partners, a boutique investment bank.
                     It converts background color of an Excel workbook based on the source and value of each cell.
                 </p>
             </div>
-            <div className="card" style={{ padding: 10, background: "#C2E1C2" }}>
-                <span className="card-title">Twitter Clone</span>
-                <p>
+            <div style={{ paddingBottom: "1%", paddingTop: "0.5%" }}>
+                <div>
+                    <p style={{ fontWeight: "bold", fontSize: "24px", marginLeft: "2%", marginBottom: "0.5%" }}>Twitter Clone</p>
+                </div>
+                <p style={{ marginTop: "0%", marginLeft: "5%", fontSize: "18px" }}>
                     Worked on as a two person team. TwitterClone is a flask server made to resemble Twitter.
                     Hosted on Google Cloud Platform across multiple instances to accommodate large amounts of traffic.
                     Wrote the frontend using pure Javascript. 
