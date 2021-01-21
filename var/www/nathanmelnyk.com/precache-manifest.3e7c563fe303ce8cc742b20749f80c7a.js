94bdba820291c44122be44c038654ee284cd5605
nmelnyk1001 <54589440+nmelnyk1001@users.noreply.github.com>
2021-01-21 16:08:28 -0500

    Changed to Build Version

---
+++ var/www/nathanmelnyk.com/precache-manifest.3e7c563fe303ce8cc742b20749f80c7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c23e385b46734dd35909114c398e105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccab0537301ec89ebaa9",
+    "url": "/static/css/main.a0f443d9.chunk.css"
+  },
+  {
+    "revision": "d8ac98fafc78931bac25",
+    "url": "/static/js/2.05ef59ce.chunk.js"
+  },
+  {
+    "revision": "bc23c04eec3c55de9f1e7db1db146add",
+    "url": "/static/js/2.05ef59ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccab0537301ec89ebaa9",
+    "url": "/static/js/main.ea6d5e53.chunk.js"
+  },
+  {
+    "revision": "5dd082ab09bb08bf7a15",
+    "url": "/static/js/runtime-main.ca548392.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "dc9190af51a9c521a3445ad92f2853a7",
+    "url": "/static/media/headshot.dc9190af.jpg"
+  }
+]);
